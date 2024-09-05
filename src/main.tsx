4adc0e3b51f8f0594cac208ea11d755a63d27f14
YangDamin <cxz26@naver.com>
2024-09-05 21:49:01 +0900

    style: 폰트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import './index.css';
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { createRoot } from 'react-dom/client';
@@ -15,8 +17,14 @@ declare module '@tanstack/react-router' {
 
 const queryClient = new QueryClient();
 
+const theme = extendTheme({
+	fonts: {
+		body: 'WantedSans, system-ui, sans-serif',
+	},
+});
+
 createRoot(document.getElementById('root')!).render(
-	<ChakraProvider>
+	<ChakraProvider theme={theme}>
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
 		</QueryClientProvider>
